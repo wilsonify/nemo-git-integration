f47d9bcfa66017fd6a96469054416cb013821dd5
wilsonify <tom.andrew.wilson@gmail.com>
2025-05-21 15:51:11 -0400

    nemo/actions

---
+++ nemo-git-integration/s01-create/s01c-branch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Create a new git branch or switch to an existing one
+
+# Navigate to the given directory
+cd "$1"
+
+# Get the branch name
+BRANCH_NAME=$(
+    # List all local and remote branches. remove unwanted characters and lines via sed.
+    # Create a drop-down list with an editable text entry.
+    # Prompt for a new branch name
+
+    (git branch -a | \
+    sed -e "s/^..//" -e "s/remotes.origin.//" -e "s/*//" -e "/HEAD detached/d" | \
+    sort -u && echo "[New Branch]"\
+    ) | \
+    zenity --list --editable \
+    --text "Select an existing branch or type a new branch name:" \
+    --title "Git Create/Switch Branch" \
+    --column "Branches"
+)
+
+# Check if the branch name is provided
+
+if [ -n "$BRANCH_NAME" ]; then
+    # If the user chose to create a new branch
+    if [ "$BRANCH_NAME" = "[New Branch]" ]; then
+        BRANCH_NAME=$(zenity --entry --text "Enter the new branch name" --title "Git Create Branch")
+        git checkout -b "$BRANCH_NAME"
+    else
+      git checkout "$BRANCH_NAME"
+    fi
+
+fi
